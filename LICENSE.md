f87ed65bce266bb1e393c5a00de6f17ad488a4d2
Mashlan, Eric <Eric.Mashlan1@T-Mobile.com>
2017-10-16 09:42:15 -0700

    init cleanup commit

--- LICENSE.md
+++ LICENSE.md
@@ -1,7 +1,7 @@
  DO WHAT THE FUCK YOU WANT TO PUBLIC LICENSE 
                     Version 1, October 2017 
 
- Copyright (C) 2017 André Davis <cubicle.jockey@live.com>, Eric Mashlan <> 
+ Copyright (C) 2017 André Davis <cubicle.jockey@live.com>, Eric Mashlan <eric.mashlan@gmail.com> 
 
  Everyone is permitted to copy and distribute verbatim or modified 
  copies of this license document, and changing it is allowed as long 
---
+++ Serilog-FakeItEasy/FakeLogger.cs
@@ -0,0 +1,111 @@
+﻿using FakeItEasy;
+using Serilog;
+using Serilog.Events;
+using System;
+using System.Linq.Expressions;
+
+namespace Sentinel.Web.Tests
+{
+    public static class FakeLogger
+    {
+        private static ILogger logger;
+
+        private static void SetLogger()
+        {
+            if (logger != null) return;
+
+            logger = A.Fake<ILogger>();
+            Log.Logger = logger;
+        }
+
+        public static Expression<Action> Information(string messageTemplate, params  object[] propertyValue)
+        {
+            return Write(LogEventLevel.Information, messageTemplate, propertyValue);
+        }
+
+        public static Expression<Action> Information(string message)
+        {
+            return Write(LogEventLevel.Information, message);
+        }
+
+        public static Expression<Action> Error(string message, Exception ex)
+        {
+            return Write(LogEventLevel.Error, ex, message);
+        }
+
+        public static Expression<Action> Error(string message)
+        {
+            return Write(LogEventLevel.Error, message);
+        }
+
+        public static Expression<Action> Error(string message, Exception ex, params object[] propertyValues)
+        {
+            return Write(LogEventLevel.Error, ex, message, propertyValues);
+        }
+
+        public static Expression<Action> Fatal(string message, params object[] propertyValues)
+        {
+            return Write(LogEventLevel.Fatal, message, propertyValues);
+        }
+
+        public static Expression<Action> Fatal(string message)
+        {
+            return Write(LogEventLevel.Fatal, message);
+        }
+
+        public static Expression<Action> Warning(string message, Exception ex)
+        {
+            return Write(LogEventLevel.Warning, ex, message);
+        }
+
+        public static Expression<Action> Warning(string message)
+        {
+            return Write(LogEventLevel.Warning, message);
+        }
+
+        public static Expression<Action> Warning(string message, Exception ex, params object[] propertyValues)
+        {
+            return Write(LogEventLevel.Warning, ex, message, propertyValues);
+        }
+
+        #region Write Messages
+        private static Expression<Action> Write(LogEventLevel logEventLevel, string messageTemplate, params object[] propertyValues)
+        {
+            SetLogger();
+            Expression<Action> logExpression = () => logger.Write(logEventLevel, messageTemplate, propertyValues);
+            A.CallTo(logExpression).DoesNothing();
+
+            return logExpression;
+        }
+
+        private static Expression<Action> Write(LogEventLevel logEventLevel, string messageTemplate)
+        {
+            SetLogger();
+            Expression<Action> logExpression = () => logger.Write(logEventLevel, messageTemplate);
+            A.CallTo(logExpression).DoesNothing();
+
+            return logExpression;
+        }
+
+        private static Expression<Action> Write(LogEventLevel logEventLevel, Exception ex, string messageTemplate, string propertyValue)
+        {
+            SetLogger();
+            Expression<Action> logExpression = () => logger.Write(logEventLevel, ex, messageTemplate, propertyValue);
+            A.CallTo(logExpression).DoesNothing();
+
+            return logExpression;
+        }
+
+        private static Expression<Action> Write(LogEventLevel logEventLevel, Exception ex, string messageTemplate, params object[] propertyValues)
+        {
+            SetLogger();
+            Expression<Action> logExpression = () => logger.Write(logEventLevel, ex, messageTemplate, propertyValues);
+            A.CallTo(logExpression).DoesNothing();
+
+            return logExpression;
+        }
+
+        #endregion
+
+    }
+}
